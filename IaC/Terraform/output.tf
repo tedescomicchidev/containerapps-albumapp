8d2710365e4cc83e9ff75762d465582e8c487649
Michele <mitedesc@microsoft.com>
2022-08-12 12:42:36 +0000

    Added workspace to Terraform

--- IaC/Terraform/output.tf
+++ IaC/Terraform/output.tf
@@ -1,3 +1,8 @@
 output "resource_group_name" {
     value = azurerm_resource_group.rg.name
 }
+
+output "log_analytics" {
+  description = "Log Analytics Workspace"
+  value       = azurerm_log_analytics_workspace.acaworkspace.name
+}
--- IaC/Terraform/variables.tf
+++ IaC/Terraform/variables.tf
@@ -57,3 +57,15 @@ variable "azure_container_registry_pwd" {
     default = "OhBbZn3gHXpkuhZqc4awUs1QL32Y=utm"
     description = ""
 }
+
+variable "deploy_location" {
+  type        = string
+  default     = "eastus"
+  description = "The Azure Region in which all resources in this example should be created."
+}
+
+variable "rg_shared_name" {
+  type        = string
+  default     = "rg-shared-resources"
+  description = "Name of the Resource group in which to deploy shared resources"
+}
